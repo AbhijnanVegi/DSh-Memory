aa8fb6af0946a9cdcdea6b2f8a1210e1417c756c
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2024-04-19 20:47:55 +0530

    Made broadcast generic

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("got /hello request\n")
-	sm.SendBroadcast()
+	// sm.SendBroadcast()
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
--- shmem/dsm.go
+++ shmem/dsm.go
@@ -4,23 +4,32 @@ import (
 	"log"
 	"sync"
 	"net/rpc"
+	"encoding/gob"
 )
 
 type RegisterReply struct {
-	Addrs []string
+	Id int
 }
 
-type BroadcastArgs struct {
-	Ptr int
+type Creds struct {
+	SenderId int
+}
+
+type RegisterArgs struct {
+	Addrs []string
+	Creds
 }
 
 type DSM struct {
+	id int
 	addrs []string
 	addrLock sync.Mutex
 }
 
 func (d *DSM) Init(serverAddress string, myAddress string) error {
 	if serverAddress == "" {
+		d.id = 0
+		d.addrs = []string{myAddress}
 		return nil
 	} else {
 		client, err := rpc.DialHTTP("tcp", serverAddress)
@@ -32,10 +41,7 @@ func (d *DSM) Init(serverAddress string, myAddress string) error {
 		if err != nil {
 			return err
 		}
-		d.addrLock.Lock()
-		defer d.addrLock.Unlock()
-		d.addrs = reply.Addrs
-		d.addrs = append(d.addrs, serverAddress)
+		d.id = reply.Id
 		log.Printf("Got addrs: %v\n", d.addrs)
 	}
 	return nil
@@ -43,19 +49,31 @@ func (d *DSM) Init(serverAddress string, myAddress string) error {
 
 func (d *DSM) RegisterReplica(addr *string, reply *RegisterReply) error {
 	log.Printf("got register from %s\n", *addr)
+	d.addrLock.Lock()
+	defer d.addrLock.Unlock()
 	d.addrs = append(d.addrs, *addr)
-	reply.Addrs = d.addrs
+	reply.Id = len(d.addrs) - 1
+	go func() {
+		d.SendBroadcast("DSM.UpdateAddrs", &RegisterArgs{Addrs: d.addrs, Creds: Creds{SenderId: d.id}})
+	}()
 	return nil
 }
 
-func (d *DSM) Update(args *BroadcastArgs, reply *int) error {
-	log.Printf("got update %s\n", args.Ptr)
+func (d *DSM) UpdateAddrs(args *RegisterArgs, reply *int) error {
+	if args.SenderId == d.id {
+		return nil
+	}
+	log.Printf("Got update addrs: %v\n", args.Addrs)
+	d.addrLock.Lock()
+	defer d.addrLock.Unlock()
+	d.addrs = args.Addrs
 	*reply = 0
 	return nil
 }
 
-func (d *DSM) SendBroadcast() {
+func (d *DSM) SendBroadcast(fun string, args interface{}) []int {
 	log.Printf("[DEBUG] Sending broadcast to %s\n", d.addrs)
+	replies := make([]int, 0)
 	for _, addr := range d.addrs {
 		client, err := rpc.DialHTTP("tcp", addr)
 		if err != nil {
@@ -63,13 +81,21 @@ func (d *DSM) SendBroadcast() {
 			continue
 		}
 		var reply int
-		args := BroadcastArgs{Ptr: 5}
-		log.Printf("[DEBUG] Sending broadcast to %s\n", addr)
-		err = client.Call("DSM.Update", &args, &reply)
+		log.Printf("[DEBUG] Calling %s(%s) on %s\n",fun, args, addr)
+		err = client.Call(fun, args, &reply)
 		if err != nil {
 			log.Printf("Error calling %s: %v\n", addr, err)
 			continue
 		}
+		replies = append(replies, reply)
 	}
+	log.Printf("[DEBUG] Got replies: %v\n", replies)
+	return replies
+}
+
+func init() {
+	gob.Register(RegisterArgs{})
+	gob.Register(RegisterReply{})
+	gob.Register(Creds{})
 }
 
